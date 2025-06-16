bad8346bc21a71c51a8937cdb5a9784001b92348
819369336@qq.com <819369336@qq.com>
2025-06-16 20:56:16 +0800

    you can add 100 clues ui

--- scripts/clue_image_ui.gd
+++ scripts/clue_image_ui.gd
@@ -4,16 +4,20 @@ extends ClueBaseUI
 class_name ClueImageUI
 
 @onready var img = %Img
-#@onready var margin_container = %MarginContainer
 
-func center_control(control: Control, size: Vector2) -> void:
-	control.set_anchors_preset(Control.PRESET_CENTER)
-	control.set_offset(SIDE_LEFT, 0)
-	control.set_offset(SIDE_RIGHT, 0)
-	control.set_offset(SIDE_TOP, 0)
-	control.set_offset(SIDE_BOTTOM, 0)
-	control.pivot_offset = Vector2.ZERO
-	control.size = size
+# 生成新实例后，不居中，重新手动刷新锚点才会居中。此为gpt给的方案
+func center_control(control: Control, size: Vector2):
+	control.set_anchors_preset(PRESET_CENTER) # 默认居中显示 只设size
+	control.size = clue_data.get("size")
+	control.anchor_left = 0.5
+	control.anchor_right = 0.5
+	control.anchor_top = 0.5
+	control.anchor_bottom = 0.5
+
+	control.offset_left = -size.x / 2
+	control.offset_right = size.x / 2
+	control.offset_top = -size.y / 2
+	control.offset_bottom = size.y / 2
 
 func _ready():
 	content_layer = %ContentLayer
@@ -23,8 +27,6 @@ func _ready():
 	super.set_clue_panel(clue_panel)
 
 	var image_data = clue_data.get("data").get("images")[0] # 应该只用一张图片
-	#self.set_anchors_preset(PRESET_CENTER) # 默认居中显示 只设size
-	#self.size = clue_data.get("size")
 	center_control(clue_panel, clue_data.get("size"))
 	var tex = load(image_data.path)
 	img.texture = tex
